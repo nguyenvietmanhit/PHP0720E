8cf77725a0d48ff904476b49affca5c084d55ddd
Manh <nvmanh@cmc.com.vn>
2020-09-15 21:14:47 +0700

    upate

---
+++ Day17_PHP_Array_Function_Array_Time/Code_demo_tren_lop/ky_nang_viet_cv_co_ban.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * ky_nang_viet_cv_co_ban.php
+ * + Kỹ năng cơ bản khi viết CV
+ *  - CẦn tìm 1 template cho CV, tham khảo trên mạng,
+ *  - Nhập các thông tin cho CV, tùy vị trí ứng tuyển mà thông
+ * tin sẽ khác nhau
+ *  - Với các fresher, chưa có kinh nghiệm thì nên tập trung
+ * vào các thông tin sau:
+ * + Mô tả chi tiết project đã làm: hệ thống dùng ngôn ngữ gì,
+ * csdl gì, các chức năng gì, có dùng Git để quản lý mã nguồn
+ * ko ... ??
+ * + Để có cơ hội đc đi phỏng vấn, thì cần là người chủ động,
+ * thông qua các nhóm trên face liên quan đến tuyển dụng php,
+ * xem các bài post, chủ động inbox người post (HR) để hỏi xem
+ * có tuyển vị trí thực tập
+ */
